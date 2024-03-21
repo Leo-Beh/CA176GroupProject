bdb325e352b30d16e8d6a03993cb77e6f6b33bd6
Leo-Beh <zi.beh2@mail.dcu.ie>
2024-03-21 20:17:54 +0000

    First upload of Q10

---
+++ 10_function_and_filter.R
@@ -0,0 +1,18 @@
+library(readxl)
+library(dplyr)
+
+companies_df <- read_excel("companies.xlsx")
+
+top_companies <- function(dataframe) {
+  
+  # filter, order and limiting the companies
+  filtered_companies_df <- dataframe %>%
+#    filter(grepl("Job Security", Highly_rated_for) & grepl("Work-Life Balance", Highly_rated_for)) %>%
+    filter(Highly_rated_for == "Job Security, Work Life Balance") %>%
+    arrange(desc(Ratings)) %>%
+    slice(1:5)
+  
+  return (filtered_companies_df[, c("Company_name", "Description")])
+}
+
+top_companies(companies_df)
