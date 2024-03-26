57f9c4be5557c659081df09a4a099aaa22a372f1
Leo-Beh <zi.beh2@mail.dcu.ie>
2024-03-26 10:13:03 +0000

    i say the last uplaod of Q9

--- 9_filters_subset_and_chart_them.R
+++ 9_filters_subset_and_chart_them.R
@@ -16,24 +16,27 @@ filtered_companies_df <- companies_df %>%
 # Creating a scatter plot to visualize the relationship between company rating and average salary
 ggplot(data = filtered_companies_df, aes(x = Avg_salary, y = Ratings)) +
   geom_point(aes(colour = Ratings), shape = 16, alpha = 0.7) +
-  geom_smooth(method = "lm", color = "black", se = TRUE) +
+  geom_smooth(method = "gam", color = "black", se = TRUE, size = 0.7) +
   scale_color_gradient(low = "blue", high = "green") +
   theme_bw() +
   
   labs(title = "Scatter Plot of Avg Salary vs Ratings",
+       subtitle = "For comapnies only rated for Job Security",
        x = "Average Salary",
-       y = "Ratings") +
+       y = "Ratings",
+       caption = "A very weak correlation between average salary and rating for companies who are only highly rated for Job security") +
   
 #  scale_x_log10(labels = scales::comma) + # Scale the x axis
   scale_x_continuous(labels = scales::comma,
                      # breaks = scales::pretty_breaks(n = ),
                      # breaks = function(x) 10^pretty(log10(range(x, finite = TRUE))),
                      trans = "log10") +
-  scale_y_continuous(limits = c(2, 5),
-                     breaks = 2:5,
-                     labels = c("2 Fair","3 Good", "4 Very Good", "5 Excellent")) +
+  scale_y_continuous(limits = c(2.5,4.5),
+                     breaks = 2.5:4.5,
+                     labels = c("2 Bad", "3 Fair", "4 Good")) +
   
-  theme(plot.title = element_text(hjust = 0.5, face = "bold.italic"),  # Center the plot title
+  theme(plot.title = element_text(hjust = 0.5, face = "bold.italic"),# Center the plot title
+        plot.subtitle = element_text(hjust = 0.5, face = "italic"),
         legend.position = "right",  # Adjust legend position
         axis.text = element_text(size = 10, face = "italic"),  # Adjust text size
         axis.title = element_text(size = 12, face = "bold.italic"))  # Adjust title size and font 
