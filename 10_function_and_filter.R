74cca1afb3c9cc02ad4caa0486e6580ceaa670fc
Leo-Beh <zi.beh2@mail.dcu.ie>
2024-04-08 09:25:59 +0100

    updated q10

--- 10_function_and_filter.R
+++ 10_function_and_filter.R
@@ -1,18 +1,23 @@
 library(readxl)
 library(dplyr)
 
+
+# Load data ---------------------------
 companies_df <- read_excel("companies.xlsx")
 
+
+# Creating function -------------------
 top_companies <- function(dataframe) {
   
   # filter, order and limiting the companies
   filtered_companies_df <- dataframe %>%
-   filter(grepl("Job Security", Highly_rated_for) & grepl("Work Life Balance", Highly_rated_for)) %>%
-#   filter(Highly_rated_for == "Job Security, Work Life Balance") %>%
+    filter(Highly_rated_for == "Job Security")%>%
     arrange(desc(Ratings)) %>%
     slice(1:5)
   
   return (filtered_companies_df)
 }
 
+
+# View filtered data -----------------
 View(top_companies(companies_df))
